9cfafb6dcf140aac3b82a5f3755aec83eeb728b0
duydodevgithub <duydo.dev@gmail.com>
2019-09-09 02:02:56 -0500

    update secure_asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,8 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production')) {
-            URL::forceSchema('https');
-        }
+        //
+        URL::forceScheme('https');
     }
 }
