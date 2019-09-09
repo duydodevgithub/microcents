3ce946accbc97c3c625911c3faab66c3ffa77037
duydodevgithub <duydo.dev@gmail.com>
2019-09-09 02:13:35 -0500

    update secure_asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         //
-        if(env('APP_ENV') == 'production')
+        if(env('APP_ENV') !== 'local')
         {
             $url->forceSchema('https');
         }
